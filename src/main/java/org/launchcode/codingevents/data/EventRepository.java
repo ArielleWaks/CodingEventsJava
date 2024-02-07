268a2e30840c182447e6c92c57599d6ecf97e983
ArielleWaks <ariellemwaks@gmail.com>
2024-02-07 17:14:06 -0400

    refactored EventController to use EventRepository  instead of EventData

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
-//    CrudRepository is a base class we are extending
+//    CrudRepository is a base interface we are extending
 //    allows us to interact w/Event objects in the database
 //    Event: what types of things/objects we are storing
 //    Integer: datatype of primary key
 
+//    since this is an interface, no instances can be created
+//    need a concrete class that implements EventRepository
+//    springboot/java creates classes on the fly and stores it in memory when app is running
 }
